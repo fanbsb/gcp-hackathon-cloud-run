014529d80fcb7bfcf1bb110bf0f9a9d778c4608d
Philip Fan <fanwengang@hotmail.com>
2022-12-01 16:10:24 +0800

    add: simple play logic

--- api/play.go
+++ api/play.go
@@ -1,27 +1,107 @@
 package api
 
 import (
-	"encoding/json"
 	"gcp-hackathon-cloud-run/types"
-	"log"
 )
 
 const (
+	// Direction
+	NORTH = "N"
+	SOUTH = "S"
+	WEST  = "W"
+	EAST  = "E"
+
+	// Command
 	FORWARD = "F"
 	LEFT    = "L"
 	RIGHT   = "R"
 	THROW   = "T"
 )
 
-var playground = [][]string{}
+var playground [][]bool
 
 func Play(input types.ArenaUpdate) string {
-	jstr, _ := json.Marshal(input)
-	log.Println(string(jstr))
+	regroup(input.Arena.Dimensions, input.Arena.State)
 
-	commands := []string{"F", "R", "L", "T"}
-	//rand := rand2.Intn(4)
+	return action(input.Arena.State[input.Links.Self.Href])
+}
 
-	// TODO add your implementation here to replace the random response
-	return commands[0]
+func regroup(ground []int, states map[string]types.PlayerState) {
+	// init the playground
+	playground = make([][]bool, ground[0])
+	for i := 0; i < ground[0]; i++ {
+		playground[i] = make([]bool, ground[1])
+	}
+
+	// fill the playground
+	for _, v := range states {
+		playground[v.X][v.Y] = true
+	}
+}
+
+func action(self types.PlayerState) string {
+	// Throw
+	if throw(self) {
+		return THROW
+	}
+	// Runaway
+	if self.WasHit {
+		return wasHit(self)
+	}
+
+	return findPlayerNearBy(self)
+}
+
+// Find the player and throw
+func throw(self types.PlayerState) bool {
+	switch self.Direction {
+	case NORTH:
+		return hasPlayer(self.X, self.Y-1)
+	case SOUTH:
+		return hasPlayer(self.X, self.Y+1)
+	case WEST:
+		return hasPlayer(self.X-1, self.Y)
+	case EAST:
+		return hasPlayer(self.X+1, self.Y)
+	}
+	return false
+}
+
+// Find the player and runaway
+func wasHit(self types.PlayerState) string {
+	switch self.Direction {
+	case NORTH:
+		return hasBlockToMove(self.X, self.Y-1)
+	case SOUTH:
+		return hasBlockToMove(self.X, self.Y+1)
+	case WEST:
+		return hasBlockToMove(self.X-1, self.Y)
+	case EAST:
+		return hasBlockToMove(self.X+1, self.Y)
+	}
+	return ""
+}
+
+// Find Player NearBy
+func findPlayerNearBy(self types.PlayerState) string {
+	//
+	return FORWARD
+}
+
+func hasPlayer(x, y int) bool {
+	if x < 0 || y < 0 {
+		return false
+	}
+	return playground[x][y]
+}
+
+func hasBlockToMove(x, y int) string {
+	if x < 0 || y < 0 {
+		return LEFT
+	}
+	if !playground[x][y] {
+		return FORWARD
+	} else {
+		return RIGHT
+	}
 }
